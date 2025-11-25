60e71866b9d621fde03df5d775ab4cd6b072f7a9
HarshDodiya1 <dodiyaharsh999@gmail.com>
2025-11-25 23:23:41 +0530

    feat: add backend CI/CD pipeline configuration

--- server/src/server.js
+++ server/src/server.js
@@ -106,7 +106,7 @@ app.use("/api/admin/payments", adminPaymentRoutes);
 
 app.get("/", (req, res) => {
   res.status(200).json({
-    message: "Welcome to the Backend APIs of HomeoPatha",
+    message: "Welcome to the Backend APIs of HomeoPathaa",
     creator: "Harsh Dodiya | Het Saraiya",
     GitHub: config.github1 || "Harsh Dodiya",
     Github: config.github2 || "Het Saraiya",
