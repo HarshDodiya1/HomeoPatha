7632ec45bb45424ec30de6230b2a9bb69f2996ba
hetsaraiya <hetsaraiya06@gmail.com>
2026-07-19 11:32:21 +0530

    feat(layout): add Facebook Pixel tracking script to layout component

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Ubuntu, Space_Grotesk } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
 import { Toaster } from "sonner";
@@ -42,6 +43,27 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <body className={`${ubuntu.variable} ${spaceGrotesk.variable} font-sans antialiased`}>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1415509880633362');
+fbq('track', 'PageView');`}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1415509880633362&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         <ScrollProgress height={4} color="bg-primary" position="top" />
         <Navbar />
         {children}
